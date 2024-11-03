966edc0f99cd79a448ff0212ded80b0391eb02ba
Golam Mustafa <golammustafa@gmail.com>
2024-11-02 22:59:33 -0400

    firebase installed

---
+++ src/firebase.cofiq.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA5ZBpMHUBPjwkXdSjXPtwyCytpoY_CPlo",
+  authDomain: "eventmanagement-4454d.firebaseapp.com",
+  projectId: "eventmanagement-4454d",
+  storageBucket: "eventmanagement-4454d.firebasestorage.app",
+  messagingSenderId: "228201037588",
+  appId: "1:228201037588:web:4a06bfbfd3ebcea6f3cc01"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
